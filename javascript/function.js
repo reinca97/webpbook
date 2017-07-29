d25ee64c4a99dbbbc12928d0c712de43ce92a163
LAPTOP-08K2TQFS\reinc <reinca97@naver.com>
2017-07-29 15:04:30 +0900

    heade: 이미지에서 태그로 수정 player_detail: 작업 시작

---
+++ javascript/function.js
@@ -0,0 +1,17 @@
+//toggle top menu
+
+$(function(){
+
+
+    $('.menu-btn').data({'clicked':true}).on('click',function(){
+
+        if($(this).data().clicked){
+            $(this).addClass('active').data({'clicked':false});
+        }else{
+            $(this).removeClass('active').data({'clicked':true});
+        }
+    })
+
+
+
+});
