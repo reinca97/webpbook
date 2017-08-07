21f72f2f67e679f02655c635b9e9c9cf2564f8af
reinca97 <reinca97@naver.com>
2017-08-07 18:34:54 +0900

    학원 작업 player_detail & program 페이지 javascript(구역 scroll)추가 program_detail 페이지 시작

---
+++ javascript_src/program.js
@@ -0,0 +1,75 @@
+/**
+ * Created by Administrator on 2017-08-07.
+ */
+
+$(function(){
+
+  //메뉴바 마우스 오버시 길이 변경
+
+  $('.program-menu-program').on('mouseenter',function(){
+
+    $('.program-menu-composer').removeClass('active');
+    $('.program-menu-program').addClass('active');
+
+  });
+
+
+  $('.program-menu-composer').on('mouseenter',function(){
+
+    $('.program-menu-program').removeClass('active');
+    $('.program-menu-composer').addClass('active');
+
+  });
+
+  $('.program-menu-composer').on('mouseleave',function(){
+    $(this).removeClass('active');
+    $('.program-menu-program').addClass('active');
+
+  });
+
+
+
+});
+
+////section 별 스크롤 동작
+
+window.onload = function () {
+  $(".move-section").each(function () {
+    // 개별적으로 Wheel 이벤트 적용
+    $(this).on("mousewheel DOMMouseScroll", function (e) {
+      e.preventDefault();
+      var delta = 0;
+      if (!event) event = window.event;
+
+
+
+          if (event.wheelDelta) {
+        delta = event.wheelDelta / 120;
+        if (window.opera) delta = -delta;
+      } else if (event.detail) delta = -event.detail / 3;
+      var moveTop = null;
+
+      // 마우스휠을 위에서 아래로
+      if (delta < 0) {
+        if ($(this).next() != undefined) {
+          moveTop = $(this).next().offset().top;
+        }
+
+        // 마우스휠을 아래에서 위로
+      } else {
+        if ($(this).prev() != undefined) {
+          moveTop = $(this).prev().offset().top;
+        }
+      }
+      // 화면 이동 0.5초
+      $("html,body").stop().animate({
+        scrollTop: moveTop + 'px'
+      }, {
+        duration: 500, complete: function () {
+        }
+      });
+    });
+  });
+
+};
+
