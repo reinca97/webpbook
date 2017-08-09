0bb278537372f1d8919cb0d78b918dcb9b6afc9a
reinca97 <reinca97@naver.com>
2017-08-09 21:56:33 +0900

    no message

--- javascript_src/player_detail.js
+++ javascript_src/player_detail.js
@@ -1,10 +1,15 @@
-///**
-// * Created by Administrator on 2017-08-09.
-// */
-//$(function(){
+/**
+ * Created by Administrator on 2017-08-09.
+ */
+$(function(){
+
+//$('.section1').on('mousewheel',function(){
 //
-//$('.introduce-text').on('mousewheel'.function(e){})
+//  $('.introduce-text').stop().animate({},500);
 //
 //
-//
-//});
+//  })
+
+
+
+});
--- javascript_src/program.js
+++ javascript_src/program.js
@@ -39,43 +39,52 @@ $(function(){
 
 ////section 별 스크롤 동작
 window.onload = function () {
-  $(".move-section").each(function () {
+  $(".move-section").each(function(){
+
     // 개별적으로 Wheel 이벤트 적용
     $(this).on("mousewheel DOMMouseScroll", function (e) {
-      e.preventDefault();
-      var delta = 0;
-      if (!event) event = window.event;
 
-          if (event.wheelDelta) {
-        delta = event.wheelDelta / 120;
-        if (window.opera) delta = -delta;
+      if( !$(e.target).parent().hasClass('inner-scroll') ){
+        e.preventDefault();
+        var delta = 0;
+        if (!event) event = window.event;
+
+        if (event.wheelDelta) {
+          delta = event.wheelDelta / 120;
+          if (window.opera) delta = -delta;
+        }
+        else if (event.detail) delta = -event.detail / 3;
+
+        var moveTop = null;
+        // 마우스휠을 위에서 아래로
+        if (delta < 0) {
+          if ($(this).next() != undefined) {
+            moveTop = $(this).next().offset().top;
+          }
+
+          // 마우스휠을 아래에서 위로
+        } else {
+          if ($(this).prev() != undefined) {
+            moveTop = $(this).prev().offset().top;
+          }
+        }
+
+        // 화면 이동 0.5초
+        $("html,body").stop().animate({
+          scrollTop: moveTop + 'px'
+        }, {
+          duration: 500, complete: function () {
+          }
+        });
+
       }
-          else if (event.detail) delta = -event.detail / 3;
-
-      var moveTop = null;
-      // 마우스휠을 위에서 아래로
-      if (delta < 0) {
-        if ($(this).next() != undefined) {
-          moveTop = $(this).next().offset().top;
-        }
-
-        // 마우스휠을 아래에서 위로
-      } else {
-        if ($(this).prev() != undefined) {
-          moveTop = $(this).prev().offset().top;
-        }
-      }
-
-      // 화면 이동 0.5초
-      $("html,body").stop().animate({
-        scrollTop: moveTop + 'px'
-      }, {
-        duration: 500, complete: function () {
-        }
-      });
 
     });
+
   });
+
+
+
 
 
 //프로그램 내 인덱스 (링크)클릭시 스크롤 애니메이션
