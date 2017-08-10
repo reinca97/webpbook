9b6d9d54126d321a503be798d4441dd8686a3feb
reinca97 <reinca97@naver.com>
2017-08-10 18:21:33 +0900

    music player HTML/CSS/java 작업 *심포니/전람회 mp3파일 추가 필요함

---
+++ javascript_src/music_player.js
@@ -0,0 +1,56 @@
+/**
+ * Created by Administrator on 2017-08-10.
+ */
+$(function(){
+
+
+  var $audio =$('.audio')[0];
+
+
+  $('.audio').on('loadedmetadata',function(){
+    console.log(Math.floor($audio.duration/60)+ ':' + Math.floor($audio.duration % 60) ); //분 : 나머지  -->yatta!
+  });
+
+  //////////// ##:## 형태로  ///////////
+  function digit(time){
+    var fixDigit;
+
+    if(time<10){fixDigit='0'+time;  //10보다 작은경우 변수에 저장
+    }else{fixDigit=time;}  //10보다 크면 걍 그대로
+    return fixDigit;}
+
+  /////////////runnung time///////////////
+
+  $('audio').on('timeupdate',function(){
+    var time= digit( Math.floor($audio.currentTime/60) )+ ':' + digit( Math.floor($audio.currentTime % 60) );
+    //분:나머지
+
+    $('.current-time').html(time); //digit 함수에 time 대입하여 출력.
+
+    //progress bar 가로길이 퍼센테이지 제어( 현재시간/전체시간 * 100)
+    var progressBar = $audio.currentTime/$audio.duration*100;
+    $('.progress .bar').css({'width': progressBar+'%'});
+  });
+
+
+  //////////버튼 클릭시 플레이+멈춤 토글/////////////////
+  $('.btn').data({'play':false}).on('click',function(){
+
+    if( $(this).data('play') == false ){
+      $audio.play();
+      $(this).removeClass('play').addClass('pause');
+      $(this).data({'play':true});
+    }else{
+      $audio.pause();
+      $(this).removeClass('pause').addClass('play');
+      $(this).data({'play':false});
+    }
+
+  });
+
+
+
+
+
+
+});//진입점 끝
